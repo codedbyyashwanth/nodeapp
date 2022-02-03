84b105047efd40450e46db4b7a0d68427281a463
Yashwanth <yashmy01@gmail.com>
2022-02-03 01:06:50 -0800

    Edited Port

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -18,4 +19,4 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(3000);
+app.listen(port);
